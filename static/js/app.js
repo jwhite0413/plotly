6e6c38740c15642767defb457808bca53565f6ed
jwhite0413 <67512469+jwhite0413@users.noreply.github.com>
2020-11-14 22:17:09 -0500

    Updated app.js
    Need to do bonus and make page pretty

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,13 @@
+// @TODO Use Functions to generate code to pull json data into html
 function getPlot(id) {
     d3.json("samples.json").then((importedData) => {
         // console.log(importedData)
         var wfreq = importedData.metadata.map(d => d.wfreq)
-            // console.log(wfreq)
+            //filter data array to isolate samples
         var samples = importedData.samples.filter(s => s.id.toString() === id)[0];
-        // console.log(ids)
+        // console.log(samples)
         var values = samples.sample_values.slice(0, 10).reverse();
         // console.log(values)
-        // var labels = importedData.samples[0].otu_labels.slice(0, 10);
-        // console.log(labels)
         //top ten otu ids
         var top_ten = (samples.otu_ids.slice(0, 10)).reverse();
         // console.log(top_ten)
@@ -16,20 +15,17 @@ function getPlot(id) {
         // console.log(OTU_id)
         var labels = samples.otu_labels.slice(0, 10);
         // console.log(labels)
-        // var ids = importedData.samples[0].otu_ids
-        // var sample = importedData.samples.s
-        // var bubblelabels = importedData.samples[0].otu_labels
-
+        //create variable trace for bar graph and set up plot with values and layout
         var trace = {
             x: values,
             y: OTU_id,
             type: 'bar',
             text: labels,
-            marker: { color: 'rgb(55,83,109)' },
+            marker: { color: 'green' },
             orientation: 'h',
         };
         var data = [trace];
-
+        //create layout for bar
         var layout = {
             title: 'Top 10 OTU',
             yaxis: {
@@ -43,17 +39,19 @@ function getPlot(id) {
             }
         };
         Plotly.newPlot("bar", data, layout);
-
+        //create second variable for bubble graph and set up plot and layout for bubble chart
         var trace1 = {
             x: samples.otu_ids,
             y: samples.sample_values,
             mode: 'markers',
             marker: {
                 size: samples.sample_values,
-                color: samples.otu_ids
+                color: samples.otu_ids,
+                colorscale: 'Rainbow'
             },
             text: samples.otu_labels
         };
+        //create layout for bubble
         var layout_2 = {
             xaxis: { title: "OTU ID" },
             height: 600,
@@ -64,32 +62,38 @@ function getPlot(id) {
         Plotly.newPlot("bubble", data1, layout_2);
     });
 }
-
+//function for the information table
 function getInfo(id) {
-    d3.json("samples.json").then((data) => {
-        var metadata = data.metadata;
-        console.log(metadata)
-        var result = metadata.filter(meta => meta.id.toString() == id)[0];
+    d3.json("samples.json").then((importedData) => {
+        //variables 
+        var metadata = importedData.metadata;
+        // console.log(metadata)
+        //isolate metadata in data array with result variable
+        var result = metadata.filter(meta => meta.id.toString() === id)[0];
         var demographicInfo = d3.select("#sample-metadata");
         demographicInfo.html("");
+        //grab data for id and append
         Object.entries(result).forEach((key) => {
             demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");
         });
     });
 }
-
+//function for changed event
 function optionChanged(id) {
     getPlot(id);
     getInfo(id);
 }
-
+//function for intializing data rendering
 function init() {
+    //variable dropdown created
     var dropdown = d3.select("#selDataset");
     d3.json("samples.json").then((importedData) => {
-        console.log(importedData)
+        // console.log(importedData)
+        //get ID to append to drop down 
         importedData.names.forEach(function(name) {
             dropdown.append("option").text(name).property("value");
         });
+        //call data to display graphs
         getPlot(importedData.names[0]);
         getInfo(importedData.names[0]);
     });
